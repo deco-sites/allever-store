eec184547081be9b1f575884c6024f21021a073f
wesley moraes <wesleymoraesserafim@gmail.com>
2024-07-11 15:29:57 -0300

    .

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -48,7 +48,7 @@ function ProductCard({
   const inStock = availability === "https://schema.org/InStock";
   const possibilities = useVariantPossibilities(hasVariant, product);
   const firstSkuVariations = Object.entries(possibilities)[0];
-  const variants = Object.entries(firstSkuVariations[1] ?? {});
+  const variants = Object.entries(firstSkuVariations?.[1] ?? {});
   const relativeUrl = relative(url);
   const percent = listPrice && price
     ? Math.round(((listPrice - price) / listPrice) * 100)
@@ -173,7 +173,7 @@ function ProductCard({
 
       {/* SKU Selector */}
       {variants.length > 1 && (
-        <ul class="flex items-center justify-start gap-2 pt-4 pb-1 pl-1">
+        <ul class="flex items-center justify-start gap-2 pt-4 pb-1 pl-1 overflow-x-auto">
           {variants.map(([value, link]) => [value, relative(link)] as const)
             .map(([value, link]) => (
               <li>
